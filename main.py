d91c6e37258cb2b8d482f295512644eb0e3f781b
Baradwaj Varadharajan <baradwav@amazon.com>
2021-04-18 15:40:46 +0530

    Second Video - Code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+list_of_usernames = list()
 
 @app.get("/home/{user_name}")
 def write_home(user_name: str, query):
@@ -9,4 +10,37 @@ def write_home(user_name: str, query):
         "Name": user_name,
         "Age": 24,
         "query": query
+    }
+
+
+@app.put("/username/{user_name}")
+def put_data(user_name: str):
+    print(user_name)
+    list_of_usernames.append(user_name)
+    return {
+        "username": user_name
+    }
+
+
+@app.post("/postData")
+def post_data(user_name: str):
+    list_of_usernames.append(user_name)
+    return {
+        "usernames":list_of_usernames
+    }
+
+
+@app.delete("/deleteData/{user_name}")
+def delete_data(user_name: str):
+    list_of_usernames.remove(user_name)
+    return {
+        "usernames": list_of_usernames
+    }
+
+
+@app.api_route("/homedata", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def handle_homedata(username: str):
+    print(username)
+    return {
+        "username":username
     }
