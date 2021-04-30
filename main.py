c6eb52cff541b8f366a5b75b417868262980ee75
Baradwaj Varadharajan <baradwav@amazon.com>
2021-04-30 11:44:20 +0530

    Sixth Video - Codermonk FastAPI

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI, Body, Request, File, UploadFile, Form
+from fastapi import FastAPI, Body, Request, File, UploadFile, Form, Depends
 from pydantic import BaseModel
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 app = FastAPI()
 
 list_of_usernames = list()
 templates = Jinja2Templates(directory="htmldirectory")
+
+oauth_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@app.post("/token")
+async def token_generate(form_data: OAuth2PasswordRequestForm = Depends()):
+    print(form_data)
+    return {"access_token": form_data.username, "token_type": "bearer"}
+
+
+@app.get("/users/profilepic")
+async def profile_pic(token: str = Depends(oauth_scheme)):
+    print(token)
+    return {
+        "user": "baradwaj",
+        "profile_pic": "my_face"
+    }
 
 
 class NameValues(BaseModel):
